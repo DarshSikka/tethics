6e7c1b6959386add8f8619e74385be245f26e846
Darsh <kapilsikka@KAPILs-Air.Dlink>
2021-08-28 16:22:36 +0530

    not found page and design improvement

--- app.ts
+++ app.ts
@@ -4,7 +4,10 @@ const path = require("path");
 const stat: string = path.join(__dirname, "static");
 app.use(express.static(stat));
 const port: string | number = process.env.PORT || 9000;
-app.get("/", (req, res) => {
+app.get("/", (req: express.Request, res: express.Response) => {
   res.sendFile(stat + "/index.html");
+});
+app.get("*", (req: express.Request, res: express.Response) => {
+  res.sendFile(stat + "/404.html");
 });
 app.listen(port, (): void => console.log(`listening on port ${port}`));
